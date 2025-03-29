d808fa5d2ccbb1ff28732d0fe6140bc217bae9e3
admin <leonaynglihao@outlook.com>
2025-03-29 17:13:04 +0800

    模拟后端数据库传输

--- Express.js
+++ Express.js
@@ -4,10 +4,35 @@ const port = 3000;
 
 // Load the JSON data
 const venders = require('./venders.json');
+const customers = require('./customers.json');
+const orders = require('./orders.json');
+const products = require('./products.json');
+const transactions = require('./transactions.json');
+const users = require('./users.json');
 
 // Define an API endpoint
 app.get('/api/venders', (req, res) => {
   res.json(venders);
+});
+
+app.get('/api/customers', (req, res) => {
+  res.json(customers);
+});
+
+app.get('/api/orders', (req, res) => {
+  res.json(orders);
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/transactions', (req, res) => {
+  res.json(transactions);
+});
+
+app.get('/api/users', (req, res) => {
+  res.json(users);
 });
 
 // Start the server
