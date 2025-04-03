84f10685aa378c1569503bf572f17677dfe15358
admin <leonaynglihao@outlook.com>
2025-04-03 13:43:41 +0800

    收尾

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@
 const CONFIG = {
   // Server base URL - can be easily changed for different environments
   SERVER_URL: 'http://192.168.1.140:8080/commerce/',
-  
+  //If you are running a local server, use 'http://localhost:8080/commerce/'
   // Add other global configuration settings as needed
 };
 window.CONFIG = CONFIG;
